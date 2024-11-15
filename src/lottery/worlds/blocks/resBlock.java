e7bc0da062573987e46a3a1c90cfbf49a6e5371f
guiY <77377005+guiYMOUR@users.noreply.github.com>
2024-11-15 15:31:25 +0800

    BE adapted

--- src/lottery/worlds/blocks/resBlock.java
+++ src/lottery/worlds/blocks/resBlock.java
@@ -1,15 +1,8 @@
 package lottery.worlds.blocks;
 
-import arc.graphics.g2d.TextureAtlas;
-import arc.graphics.g2d.TextureRegion;
-import arc.scene.style.Drawable;
-import arc.scene.style.TextureRegionDrawable;
-import mindustry.content.Items;
 import mindustry.type.Category;
 import mindustry.type.Item;
 import mindustry.type.ItemStack;
-import mindustry.ui.ItemDisplay;
-import mindustry.ui.ItemImage;
 import mindustry.world.Block;
 import mindustry.world.meta.BuildVisibility;
 
